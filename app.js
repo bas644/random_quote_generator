a07affe79cc4bda1d5283f0c79e4db4d3a5be169
Riedinger <SDRiedinger@mdanderson.org>
2022-04-04 22:52:45 -0500

    adds bonus

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
 const auth = document.getElementById('randoQuoteAuth');
 const quote = document.getElementById('randoQuote');
 const quoteGen = document.getElementById('quoteGen');
+const specAuth = document.getElementById('authors');
+let stuff = [];
 
-async function getQuotes(){
-    const response = await fetch("https://type.fit/api/quotes");
-    const allQuotes = await response.json();
-    let randomizer = Math.floor(Math.random() * allQuotes.length);
-    console.log(allQuotes[randomizer].author);
-    quote.innerHTML = allQuotes[randomizer].text;
-    if(allQuotes[randomizer].author === null) {
-        auth.innerHTML = "Unknown";
-    } else {
-        auth.innerHTML = allQuotes[randomizer].author;
+function stupidFunction() {
+    for(a in stuff){
+        if(stuff[a].author === null) {
+            stuff[a].author = 'Unknown';
+        }
     }
-    
+    let moreAuthors = []
+    for(a in stuff){
+        moreAuthors.push(stuff[a].author);
+    }
+    let stupidAuthors = new Set(moreAuthors)
+    for(const a of stupidAuthors.values()) {
+            const node = new Option(a, a);
+            specAuth.appendChild(node);
+        }
 }
 
-quoteGen.addEventListener('click', getQuotes);
+async function getQuotes(writer=""){
+    const response = await fetch("https://type.fit/api/quotes");
+    const allQuotes = await response.json();
+    stuff = allQuotes;
+    stupidFunction();
+}
+
+function hereIsYourQuoteSIR(writer=""){
+    // generate new quote
+    let randomizer = Math.floor(Math.random() * stuff.length);
+    if(writer === '') {
+        quote.innerHTML = stuff[randomizer].text;
+        auth.innerHTML = stuff[randomizer].author;
+    } else if(stuff[randomizer].author === writer){
+        quote.innerHTML = stuff[randomizer].text;
+        auth.innerHTML = stuff[randomizer].author;
+    } else {
+        hereIsYourQuoteSIR(writer);
+    }  
+}
+          
+
+
+function findQuoteByAuthor(){
+    console.log(specAuth.value);
+    hereIsYourQuoteSIR(specAuth.value);
+}
+
+quoteGen.addEventListener('click', findQuoteByAuthor);
+getQuotes();
